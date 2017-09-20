e579f7dc12d9d264667605ea374b6cb489da5dad
Emily Wells Jimenez and James Blocker <james@>
2017-09-20 15:00:16 -0700

    begin business logic, if first vowel then add way, if consonent, do nothing, return results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
-
+function pigLatin(sentence) {
+  var arrayInitial = sentence.split(" ");
+  var arrayFinal = [];
+  arrayInitial.forEach(function(string) {
+    if (string.match(/[0-9]/g)) {
+      alert("Try again! Please write a sentence without numbers.")
+    } else if (string[0].match(/[aeiou]/gi)) {
+      sentence = string + "way";
+      arrayFinal.push(sentence);
+    } else if (!string[0].match(/[aeiou]/gi)) {
+      arrayFinal.push(sentence);
+    }
+  })
+  return arrayFinal;
+  }
 
 // jQuery UI logic is below
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#pigform").submit(function(event) {
       event.preventDefault();
       var sentence = $("#inputForm").val();
-      var result = pigLatin(sentence);
 
-      $(".translation").text(result);
+      $("#result").text(pigLatin(sentence));
 
-);
+  });
+});
