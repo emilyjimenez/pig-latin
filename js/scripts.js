f9e7b13043baf86bb57426e7d69e100a150946a1
Emily Wells Jimenez and James Osborn <james@>
2017-09-21 14:23:05 -0700

    begin logic for if consonant q is followed by u then add qu at end and watch out for words like squeak.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// Businnes Logic below:
 function pigLatin(sentence) {
   var arrayInitial = sentence.split(" ");
   var arrayFinal = [];
@@ -7,40 +7,38 @@ function pigLatin(sentence) {
     // Alert: dont use numbers
     if (string.match(/[0-9]/g)) {
       alert("Try again! Please write a sentence without numbers.")
-      // if vowel + "way"
+      // if vowel then string + "way"
     } else if (string[0].match(/[aeiou]/gi)) {
       sentence = string + "way";
       arrayFinal.push(sentence);
-      // if consonent + "ay plus consonentant"
+      // if consonant then consonant plus "ay"
     } else {
       for (var i = 0; i < string.length; i++) {
         console.log(i);
         if (!string[0].match(/[aeiou]/gi)) {
-         string = string.slice(1, string.length) + string.charAt(0);
-         sentence = string + "ay";
-         console.log(string);
-         console.log(sentence);
-
-
-
-
+          string = string.slice(1, string.length) + string.charAt(0);
+          sentence = string + "ay";
+          console.log(string);
+          console.log(sentence);
+          // if "qu" then "qu" at end plus "ay"
+          } else if (string[0].match(/(qu)/gi)) {
+            string = string.slice(1, string.length) + string.charAt(0);
+            sentence = string + "ay";
+            console.log(string);
+            console.log(sentence);
+          }
+        }
       }
-    }
     arrayFinal.push(sentence);
-  }
-  });
-
+    });
   return arrayFinal;
-
   }
-
-// jQuery UI logic is below
+// UI logic below:
 $(document).ready(function() {
   $("form#pigform").submit(function(event) {
       event.preventDefault();
       var sentence = $("#inputForm").val();
 
       $("#result").text(pigLatin(sentence));
-
+    });
   });
-});
