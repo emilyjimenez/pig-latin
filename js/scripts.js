708ddad430446f3f034fc02301a865bed3bef734
Emily Wells Jimenez and James Blocker <james@>
2017-09-20 18:11:46 -0700

    if consonent almost working, need to make 2 or more consonents go to the end but can only get one at the moment.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,21 @@ function pigLatin(sentence) {
   var arrayInitial = sentence.split(" ");
   var arrayFinal = [];
   arrayInitial.forEach(function(string) {
+    // Alert: dont use numbers
     if (string.match(/[0-9]/g)) {
       alert("Try again! Please write a sentence without numbers.")
+      // if vowel + "way"
     } else if (string[0].match(/[aeiou]/gi)) {
       sentence = string + "way";
       arrayFinal.push(sentence);
+      // if consonent + "ay plus consonentant"
     } else if (!string[0].match(/[aeiou]/gi)) {
+      sentence = string.slice(1, string.length) + string.charAt(0) + "ay";
       arrayFinal.push(sentence);
+
+    } else if (!string[0].match(/[aeiou]/gi) && (!string[0].match(/[aeiou]/gi))) {
+        sentence = string.slice(2, string.length) + string.charAt(1) + "ay";
+        arrayFinal.push(sentence);
     }
   })
   return arrayFinal;
